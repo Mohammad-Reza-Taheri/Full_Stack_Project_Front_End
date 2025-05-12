d6a1ed9becf5a521b499d5b6ee58801ad082ae76
Mohamad Reza <hossinfe1@gmail.com>
2025-05-12 23:49:21 +0330

    fix color test1

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,23 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  // plugins: ["@tailwindcss/postcss"],
+   plugins: {
+    '@tailwindcss/postcss': {
+       darkMode:'class'
+    },
+  },
+  
 };
 
+// module.exports={
+//   darkMode:'class'
+// }
+
 export default config;
+
+
+
+// export default {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//   },
+// }
