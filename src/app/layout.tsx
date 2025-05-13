37e2fddf532327c213802d988323d9a2329d994e
Mohamad Reza <hossinfe1@gmail.com>
2025-05-13 06:42:11 +0330

    test for fixing the page 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="min-h-max">
       {/* <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body> */}
