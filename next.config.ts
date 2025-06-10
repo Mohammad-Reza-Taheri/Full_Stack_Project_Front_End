f3ada83fc9e136df2ba6bd98f993ca5be8e23067
Mohamad Reza <hossinfe1@gmail.com>
2025-06-10 20:56:19 +0330

    change next.config routes

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,30 @@
-import type { NextConfig } from "next";
+// /** @type {import('next').NextConfig} */ 
 
-//  /** @type {import('next').NextConfig} */ 
+
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   async redirects() {
     return [
       {
-        source: '/edit_category',
-        destination: '/',
+        source: 'https://ankibro.liara.run/edit_category',
+        destination: 'https://ankibro.liara.run/',
         permanent: false, // Set to false if you want a temporary redirect
       },
       {
-        source: '/category',
-        destination: '/',
+        source: 'https://ankibro.liara.run/category',
+        destination: 'https://ankibro.liara.run/',
         permanent: false, // Set to false if you want a temporary redirect
       },
-         {
-        source: '/category/:category_id/cards',
-        destination: '/category/:category_id',
+      {
+        source: 'https://ankibro.liara.run/category/:category_id/cards',
+        destination: 'https://ankibro.liara.run/category/:category_id',
         permanent: false, // Set to false if you want a temporary redirect
       },
-        {
-        source: '/category/:category_id/cards/edit_card',
-        destination: '/category/:category_id',
+      {
+        source: 'https://ankibro.liara.run/category/:category_id/cards/edit_card',
+        destination: 'https://ankibro.liara.run/category/:category_id',
         permanent: false, // Set to false if you want a temporary redirect
       },
     ];
